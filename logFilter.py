0da053f2c6519f84158724f47c75d7de07da504d
Meowzz <jjzzzmd@gmail.com>
2017-06-15 16:13:18 +0800

    a few improvements
    now old processed file will be removed before processing the new log file
    
    log file is match using regex now
    
    fix a bug that filter the old process in the same log. eg: the app runs more than once so there are multiple process created, the filter now correctly capture the latest run

--- logFilter.py
+++ logFilter.py
@@ -1,3 +1,4 @@
+import os
 from typing import IO, List
 import re
 
@@ -11,23 +12,25 @@ blockCount=0
 
 
 def findAppPid(f: IO):
+    pidList=[] #type:List[str]
     for line in f:
         if APP_SIGNATURE in line:
             # print(line)
-            pid = str(re.search("\(\d+\)", line).group(0))
+            pid = str(re.search("\(.+\)", line).group(0))
             print("APP PID found = %s" % pid)
-            return pid
-
+            pidList.append(pid)
+    print("Found %d pids taking last one %s" % (len(pidList),pidList[len(pidList)-1]))
+    return pidList[len(pidList)-1]
 
 
 def processBlock(block: List[str]):
     global blockCount
     blockCount+=1
     if pid in block[0]:
-        print("Block accepted")
+        #print("Block accepted")
         result.extend(block)
-    else:
-        print("Block rejected")
+    # else:
+    #     print("Block rejected")
 
 def isBlockStart(line:str):
     for p in PREFIX:
@@ -37,26 +40,47 @@ def isBlockStart(line:str):
     #print(line)
     return False
 
+def get_log_file()->str:
+    fileList = os.listdir(".")
+    for f in fileList:
+        match=re.search("log.*\.txt",f)
+        if match:
+            print("Found log file %s"%match.group(0))
+            return match.group(0)
+def clear_processed_file():
+    oldFileName="logProcessed.txt"
+    if os.path.isfile(oldFileName):
+        os.remove(oldFileName)
+        print("old log file removed")
+    else:
+        print("old log file does not exist")
 
-with open("log.txt", encoding="utf8") as f:
-    pid = findAppPid(f)
-    f.seek(0)
-    lineCount=0
-    block=[] #type:List[str]
-    for line in f:
-        if isBlockStart(line):
-            processBlock(block)
-            block.clear()
-        block.append(line)
-        lineCount+=1
 
-    with open("logProcessed.txt","w") as out:
-        for line in result:
-            out.write(line)
+if __name__=="__main__":
 
-    print(result)
-    print("Result line count=%d"%len(result))
-    print("Source Linecount=%d"%lineCount)
-    print("Source Blockcount=%d"%blockCount)
+    clear_processed_file()
+    with open(get_log_file(), encoding="utf8") as f:
+        pid = findAppPid(f)
+        f.seek(0)
+        lineCount = 0
+        block = ["empty"]  # type:List[str]
+        for line in f:
+            if isBlockStart(line):
+                processBlock(block)
+                block.clear()
+            block.append(line)
+            lineCount += 1
+
+        with open("logProcessed.txt", "w") as out:
+            for line in result:
+                out.write(line)
+
+        print(result)
+        print("Result line count=%d" % len(result))
+        print("Source Linecount=%d" % lineCount)
+        print("Source Blockcount=%d" % blockCount)
+
+
+
 
 
